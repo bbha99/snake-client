be8b94bfee42812108efe763da1e3a82395d1ef4
bbha99 <branha99@gmail.com>
2023-01-18 15:33:27 -0800

    added a name

--- client.js
+++ client.js
@@ -9,7 +9,14 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  
+
+  conn.write("Name: BHA");
+  // conn.write("Move: up");
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
   conn.on('data', (data) => {
     console.log(data.toString());
   });
